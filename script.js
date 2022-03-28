76cb3dff5a7b4eac2fe595657b1f18456112ee2f
Mario Rovito <alessandro.r14@gmail.com>
2022-03-28 16:04:39 +0200

    add bonus and addnewtodo funct

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ const app = new Vue({
     // collego vue all'html tramite l'id todo
     el: '#todo',
     data:{
+        /*
+MILESTONE 1
+Stampare all'interno di una lista, un item per ogni todo.
+Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+*/
         //creo una lista di oggetti con due proprietà
         todoList:[
             {
@@ -19,31 +24,31 @@ const app = new Vue({
             text: 'andare in palestra',
             done: false,  
             },
-        ]
+        ],
+        newTodo:'',
     },
     methods:{
 /*
 MILESTONE 2
 Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
 */
-
-// funzione inserita all'interno della i. Rimuove l'item dalla lista al click 
+// funzione inserita all'interno della i. Rimuove l'item dalla lista al click uno per volta
         deleteItem(index){
                 this.todoList.splice(index, 1);
         },
-        
-    }
-})
-
-/*
-MILESTONE 1
-Stampare all'interno di una lista, un item per ogni todo.
-Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
-*/
-
-
-
 /*
 MILESTONE 3
 Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 */
+        addNewTodo(){
+
+            if(this.newTodo.trim().length){
+               this.todoList.push({
+                  text: this.newTodo,
+                  done: false,
+               }),
+               this.newTodo = '';
+            }
+         },
+    }
+})
